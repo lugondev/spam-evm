58adb8c312536e06e864eb19dca3b93c5930f747
lugondev <lugondev@gmail.com>
2025-04-17 15:33:55 +0700

    perf: optimize faucet script for large batches
    - Add chunking to process recipients in groups of 500
    - Add lodash dependency for array chunking
    - Update package dependencies

--- scripts/faucet.ts
+++ scripts/faucet.ts
@@ -3,12 +3,13 @@ import fs from "fs";
 import deployedContracts from "../contracts_deployed.json";
 import { loadConfig } from "../config/hardhat";
 import { Disperse__factory } from "../typechain-types";
+import { chunk } from "lodash";
 
 async function main() {
 	const { keysFile, amountFaucet } = loadConfig();
 	// read the private keys from the file
 	const keys = fs.readFileSync(keysFile, "utf8").split("\n").filter(Boolean);
-	const recipients = keys.map((key) => {
+	const recipientsList = keys.map((key) => {
 		const wallet = new ethers.Wallet(key);
 		const address = wallet.address;
 		return {
@@ -16,39 +17,42 @@ async function main() {
 			amount: amountFaucet,
 		};
 	});
-	const totalValue = amountFaucet * BigInt(recipients.length);
 
 	// create a wallet faucet
 	const [faucetWallet] = await ethers.getSigners();
-	// get the balance of the faucet wallet
-	const balance = await faucetWallet.provider.getBalance(faucetWallet.address);
-	if (balance < totalValue) {
-		console.error(
-			`Faucet wallet balance is ${ethers.formatEther(
-				balance
-			)} ETH, but needs ${ethers.formatEther(totalValue)} ETH`
-		);
-		return;
-	} else {
-		console.log(
-			`Faucet wallet balance is ${ethers.formatEther(
-				balance
-			)} ETH, enough to send ${ethers.formatEther(totalValue)} ETH`
-		);
-	}
-
 	// create a contract instance
 	const disperseAddress = deployedContracts["Disperse"];
 	const disperseContract = Disperse__factory.connect(disperseAddress, faucetWallet);
-	// loop through the keys and send 0.01 ETH to each address
-	const tx = await disperseContract.disperseEther(
-		recipients.map((r) => r.address), recipients.map((r) => r.amount),
-		{
-			value: totalValue,
+
+	const chunkedRecipients = chunk(recipientsList, 500);
+	for (const recipients of chunkedRecipients) {
+		const totalValue = amountFaucet * BigInt(recipients.length);
+		// get the balance of the faucet wallet
+		const balance = await faucetWallet.provider.getBalance(faucetWallet.address);
+		if (balance < totalValue) {
+			console.error(
+				`Faucet wallet balance is ${ethers.formatEther(
+					balance
+				)} ETH, but needs ${ethers.formatEther(totalValue)} ETH`
+			);
+			return;
+		} else {
+			console.log(
+				`Faucet wallet balance is ${ethers.formatEther(
+					balance
+				)} ETH, enough to send ${ethers.formatEther(totalValue)} ETH`
+			);
 		}
-	);
-	await tx.wait();
-	console.log(`Transaction hash: ${tx.hash}`);
+
+		const tx = await disperseContract.disperseEther(
+			recipients.map((r) => r.address), recipients.map((r) => r.amount),
+			{
+				value: totalValue,
+			}
+		);
+		await tx.wait();
+		console.log(`Transaction hash: ${tx.hash}`);
+	}
 }
 
 main().catch((error) => {
